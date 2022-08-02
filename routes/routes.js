6060c9735642f5c1b01de4809f1054800b4181d5
fernando <frojasf19@gmail.com>
2022-08-02 14:31:05 -0400

    ultimos detalles

--- routes/routes.js
+++ routes/routes.js
@@ -1,14 +1,17 @@
 import express from 'express'
-import { actualizarPersonaje, crearPersonaje, mostrarPersonaje, mostrarPersonajes } from '../controller/controladorPersonajes.js'
+import { actualizarPersonaje, crearPersonaje, eliminarPersonaje, mostrarPersonaje, mostrarPersonajes } from '../controller/controladorPersonajes.js'
 
 const router = express()
 
 //definimos las rutas
 router.get('/', mostrarPersonajes)
+
 router.get('/:id', mostrarPersonaje)
 
 router.post('/', crearPersonaje)
 
 router.put('/:id', actualizarPersonaje)
+
+router.delete('/:id', eliminarPersonaje)
 
 export default router
