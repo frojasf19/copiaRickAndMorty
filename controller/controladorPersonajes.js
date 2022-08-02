d32b72afc00856a2f63d3ea2e821d6662ca63b72
fernando <frojasf19@gmail.com>
2022-08-02 14:24:35 -0400

    Agregando personajes y añadiendo mas controladores

--- controller/controladorPersonajes.js
+++ controller/controladorPersonajes.js
@@ -10,6 +10,19 @@ export const mostrarPersonajes = async (req, res) => {
         res.json({message: error.message})
     }
 }
+//mostrar un personaje
+export const mostrarPersonaje = async(req, res) => {
+    try {
+        const personaje = await personajes.findAll({
+            where:{
+                id: req.params.id
+            }
+        })
+        res.json(personaje)
+    } catch (error) {
+        res.json({message: error.message})
+    }
+}
 //crear un nuevo personaje
 export const crearPersonaje = async (req, res) => {
     try {
@@ -17,5 +30,28 @@ export const crearPersonaje = async (req, res) => {
         res.json({message: 'Personaje creado con exito'})
     } catch (error) {
         res.json({message: 'Error al crear personaje'})
+    }
+}
+//actualizar un personaje
+export const actualizarPersonaje = async (req, res) => {
+    try {
+        await personajes.update(req.body,{
+            where:{
+                id: req.params.id 
+            }
+        })
+        res.json({message: 'Personaje Actualizado'})
+    } catch (error) {
+        res.json({message: error.message})
+    }
+}
+
+//eliminar un personaje
+export const eliminarPersonaje = async (req, res) => {
+    try {
+        await personajes.destroy(req.params.id)
+        res.json({message: 'Personaje eliminado con exito'})
+    } catch (error) {
+        res.json({message: error.message})
     }
 }
--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,14 @@
 import express from 'express'
-import { crearPersonaje, mostrarPersonajes } from '../controller/controladorPersonajes.js'
+import { actualizarPersonaje, crearPersonaje, mostrarPersonaje, mostrarPersonajes } from '../controller/controladorPersonajes.js'
 
 const router = express()
 
 //definimos las rutas
 router.get('/', mostrarPersonajes)
+router.get('/:id', mostrarPersonaje)
 
 router.post('/', crearPersonaje)
 
+router.put('/:id', actualizarPersonaje)
 
 export default router
